4458bc326be5961af38739ed90b7fa6e0a720cba
numberoneecsstudent <32531002+numberoneecsstudent@users.noreply.github.com>
2019-05-31 08:56:20 -0700

    finished the server file

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ I need this code, just don't know where, perhaps should make some middleware, do
 
 Go code!
 */
+
+const server = require ('./server')
+const PORT = process.env.PORT || 8000
+
+server.listen(PORT , () =>{
+    console.log(' listening to port 8000 ')
+})
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const server = express()
+const actionRouter = require('./actionRouter')
+const projectRouter = require('./projectRouter')
+
+server.use(express.json())
+// Routes
+server.use('/aplications', actionRouter)
+server.use('/api/projects', projectRouter)
+
+server.get("/", (req, res) =>{
+    res.send("hello")
+})
+
+module.exports = server;
